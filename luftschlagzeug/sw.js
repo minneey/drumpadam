cc8fc46a92b7d24c117b75fb180942b29c57933e
minneey <SPanzner@proton.me>
2026-08-10 23:03:32 +0200

    Extract luftschlagzeug files from ZIP - add individual files

---
+++ luftschlagzeug/sw.js
@@ -0,0 +1,80 @@
+const CACHE_NAME = 'luftschlagzeug-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/icon-512-maskable.png'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache).catch(err => {
+          console.log('Cache addAll error:', err);
+          return Promise.resolve();
+        });
+      })
+      .catch(err => {
+        console.log('Cache open error:', err);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activate service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  // Skip non-GET requests
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(response => {
+          // Don't cache non-successful responses
+          if (!response || response.status !== 200 || response.type === 'error') {
+            return response;
+          }
+
+          // Clone the response
+          const responseToCache = response.clone();
+
+          // Cache successful responses
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+      .catch(() => {
+        // Return offline page or cached response
+        return caches.match('/index.html');
+      })
+  );
+});
